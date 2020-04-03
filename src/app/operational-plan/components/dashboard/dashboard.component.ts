967a7a1cd41091770873604d3507480688a2dd12
Divakar Kumar <imdivakar92@gmail.com>
2020-04-03 15:07:54 +0530

    add plan component init

--- src/app/operational-plan/components/dashboard/dashboard.component.ts
+++ src/app/operational-plan/components/dashboard/dashboard.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  connectivityMonitoringRouteList: IRouteList[] = [
+  operationalPlanMainRouteList: IRouteList[] = [
     {
       label: 'Operational Plan',
       route: '/operational-plan'
+    }
+  ];
+  operationalPlanSubRouteList: IRouteList[] = [
+    {
+      label: 'Add Plan',
+      route: '/operational-plan/plan'
+    },
+    {
+      label: 'Add Operator',
+      route: '/operational-plan/operator/add'
+    },
+    {
+      label: 'Add Operation Type',
+      route: '/operational-plan/operation-type/add'
+    },
+    {
+      label: 'Add Vessel',
+      route: '/operational-plan/vessel/add'
+    },
+    {
+      label: 'Add Robot System',
+      route: '/operational-plan/robot-system/add'
     }
   ];
 
--- src/app/operational-plan/components/manage-plans/manage-plans.component.ts
+++ src/app/operational-plan/components/manage-plans/manage-plans.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OperationalPlanService } from 'src/app/services/operational-plan.service';
 
 @Component({
   selector: 'app-manage-plans',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagePlansComponent implements OnInit {
 
-  constructor() { }
+  operationalPlansList: IOperationalPlan[] = [];
+  cols = [
+    { field: 'VesselName', header: 'Vessel', filterMatchMode: 'contains' },
+    { field: 'ImoNumber', header: 'IMO', filterMatchMode: 'contains' },
+    { field: 'OperationDate', header: 'Operation Date (Local Time)', filterMatchMode: 'contains' },
+    { field: 'ETADate', header: 'Vessel ETA Date (Local Time)', filterMatchMode: 'contains' },
+    { field: 'OperationLoc', header: 'Location', filterMatchMode: 'contains' },
+    { field: 'OperationType', header: 'Operation Type', filterMatchMode: 'contains' },
+    { field: 'OperationDes', header: 'Description', filterMatchMode: 'contains' },
+    { field: 'Status', header: 'Status', filterMatchMode: 'contains' },
+    { field: 'OperatorName', header: 'Operator', filterMatchMode: 'contains' },
+    { field: 'Planner', header: 'Planner', filterMatchMode: 'contains' },
+    { field: 'CreatedBy', header: 'Created By', filterMatchMode: 'contains' },
+    { field: 'LastUpdatedBy', header: 'Last Updated By', filterMatchMode: 'contains' },
+    { field: 'LastUpdatedDate', header: 'Updated Date (UTC Time)', filterMatchMode: 'contains' },
+    { field: 'PlanId', header: 'Action', filterMatchMode: 'contains' }
+  ];
 
-  ngOnInit() {
+  constructor(
+    private operationalPlanService: OperationalPlanService
+  ) { }
+
+  ngOnInit(): void {
+    this.getPlanList();
+  }
+
+  getPlanList(): void {
+    this.operationalPlansList = this.operationalPlanService.getOperationPlans();
   }
 
 }
