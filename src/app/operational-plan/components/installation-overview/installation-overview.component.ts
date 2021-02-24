19aef75e0dee68bde038823eb282f993fe193549
prabhu <prabhu.ravi@kdi.kongsberg.com>
2021-02-24 20:12:01 +0530

    bug fixes

--- src/app/operational-plan/components/installation-overview/installation-overview.component.ts
+++ src/app/operational-plan/components/installation-overview/installation-overview.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Table } from 'primeng/components/table/table';
+import { AppConstants } from 'src/app/app.constants';
+
 
 @Component({
   selector: 'app-installation-overview',
@@ -9,20 +12,20 @@ import { Router } from '@angular/router';
 export class InstallationOverviewComponent implements OnInit {
 
   installationList: IInstallationOverview[] = [];
+  PRIMENG_CONSTANTS = AppConstants.PRIMENG_CONSTANTS;
 
   cols = [
-    { field: 'Name', header: 'Name' },
-    { field: 'FoulingState', header: 'Fouling State' },
-    { field: 'InstallationStatus', header: 'Installation Status' },
-    { field: 'Status', header: 'Status' },
-    { field: 'Date', header: 'Date' },
-    { field: 'Port', header: 'Port' },
-    { field: 'ConnectivityStatus', header: 'ConnectivityStatus' },
-    { field: 'ETA', header: 'ETA' },
-    { field: 'ETB', header: 'ETB' },
+    { field: 'Name', header: 'Name' , sortfield: 'Name', filterMatchMode: 'contains'  },
+    { field: 'FoulingState', header: 'Fouling State' ,  sortfield: 'FoulingState', filterMatchMode: 'contains'},
+    { field: 'InstallationStatus', header: 'Installation Status',  sortfield: 'InstallationStatus', filterMatchMode: 'contains' },
+    { field: 'Status', header: 'Status',  sortfield: 'Status', filterMatchMode: 'contains' },
+    { field: 'Date', header: 'Date' , sortfield: 'Date'},
+    { field: 'Port', header: 'Port',  sortfield: 'Port', filterMatchMode: 'contains' },
+    { field: 'ConnectivityStatus', header: 'ConnectivityStatus', sortfield: 'ConnectivityStatus', filterMatchMode: 'contains' },
+    { field: 'ETA', header: 'ETA', sortfield: 'ETA' },
+    { field: 'ETB', header: 'ETB',  sortfield: 'ETB'},
     { field: 'CurrentPosition', header: 'CurrentPosition' }
   ];
-
   constructor(private router: Router) { }
 
   ngOnInit() {
